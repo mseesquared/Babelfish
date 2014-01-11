f30b4456282b28b6ffbace283f2ba00c8a35cdf5
Howon Lee <howon@howonlee.com>
2014-01-10 20:03:00 -0800

    modifications made

---
+++ README.md
@@ -0,0 +1,2 @@
+pip install microsofttranslator
+pip install web.py
--- translate.py
+++ translate.py
@@ -1,10 +1,21 @@
-import json
+import simplejson
 import urllib2
 import urllib
+import web
+from microsofttranslator import Translator
 
-transurl = "https://www.googleapis.com/language/translate/v2/detect"
-transurl += "?q="
-transurl += urllib.quote("el vida del amor")
-transurl += "&key=AIzaSyD2A3vcvZ5rot4iBrxFG7Vup5bdI8AgQOM"
-print transurl
-print json.load(urllib2.urlopen(transurl))
+urls = (
+        '/trans/(.*)', 'translate'
+        )
+
+app = web.application(urls, globals())
+
+class translate:
+    def GET(self, name):
+        translator = Translator('translatewinterhack', 'm5z06/zsCyf2mtdXfVbc2pvCSDO1wF5S1VB6cDauLIE=')
+        if not name:
+            name = "World"
+        return simplejson.dumps(translator.translate(name, "es"))
+
+if __name__ == "__main__":
+    app.run();
